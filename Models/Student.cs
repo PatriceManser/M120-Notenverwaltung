be575b65534a628a5d8e6b587226d2c62467f3b4
PatriceManser <59052834+PatriceManser@users.noreply.github.com>
2021-11-25 13:43:52 +0100

    added User, Teacher & Student Class

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notenverwaltung.Models
+{
+    public class Student : User
+    {
+        public String SchoolClass { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notenverwaltung.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String EMail { get; set; }
+        public String Password { get; set; }
+    }
+}
