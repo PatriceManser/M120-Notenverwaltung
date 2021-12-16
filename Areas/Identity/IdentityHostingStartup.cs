cad45b8113485f44471118566312e93fbdceb015
PatriceManser <59052834+PatriceManser@users.noreply.github.com>
2021-12-16 14:36:55 +0100

    NOT WORKING! Needs to find way to use same database for login and userslist

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notenverwaltung.Data;
+
+[assembly: HostingStartup(typeof(Notenverwaltung.Areas.Identity.IdentityHostingStartup))]
+namespace Notenverwaltung.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
